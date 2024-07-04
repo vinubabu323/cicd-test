55e983788087f384408ebd62fdcc8caecb94f07c
Vinu Babu <vinu.b@webandcrafts.in>
2024-07-04 15:30:33 +0530

    test 7

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const express = require("express");
-const app = express();
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
-app.get("/", (req, res) => {
-  res.status(200).send("Hello, world!");
+const app = express();
+const port = 3000;
+
+// Middleware
+app.use(bodyParser.json());
+
+//Connec to MongoDB (replace 'mongodb://localhost:27017/test' with your MongoDB URI)
+mongoose.connect("mongodb://localhost:27017/cicd-test");
+
+// Check for DB connection
+mongoose.connection.once("open", () => {
+  console.log("Connected to MongoDB");
 });
 
-module.exports = app;
+// Start server
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
---
+++ routes/route.js
@@ -0,0 +1,81 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../models/User");
+
+// Create a new user
+router.post("/users", async (req, res) => {
+  try {
+    const user = new User(req.body);
+    await user.save();
+    res.status(201).send(user);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+// Get all users
+router.get("/users", async (req, res) => {
+  try {
+    const users = await User.find();
+    res.status(200).send(users);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+// Get a user by ID
+router.get("/users/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) {
+      return res.status(404).send();
+    }
+    res.status(200).send(user);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+// Update a user by ID
+router.patch("/users/:id", async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ["name", "email", "age"];
+  const isValidOperation = updates.every((update) =>
+    allowedUpdates.includes(update)
+  );
+
+  if (!isValidOperation) {
+    return res.status(400).send({ error: "Invalid updates!" });
+  }
+
+  try {
+    const user = await User.findById(req.params.id);
+
+    if (!user) {
+      return res.status(404).send();
+    }
+
+    updates.forEach((update) => (user[update] = req.body[update]));
+    await user.save();
+    res.status(200).send(user);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+// Delete a user by ID
+router.delete("/users/:id", async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+
+    if (!user) {
+      return res.status(404).send();
+    }
+
+    res.status(200).send(user);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+module.exports = router;
