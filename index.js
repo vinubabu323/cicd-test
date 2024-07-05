9ea27d88e82d6bbda397f2ce569265c9a7a57d3b
Vinu Babu <vinu.b@webandcrafts.in>
2024-07-05 11:20:27 +0530

    db connect off

--- index.js
+++ index.js
@@ -8,13 +8,13 @@ const port = 3000;
 // Middleware
 app.use(bodyParser.json());
 
-//Connec to MongoDB (replace 'mongodb://localhost:27017/test' with your MongoDB URI)
-mongoose.connect("mongodb://localhost:27017/cicd-test");
+// //Connec to MongoDB (replace 'mongodb://localhost:27017/test' with your MongoDB URI)
+// mongoose.connect("mongodb://localhost:27017/cicd-test");
 
-// Check for DB connection
-mongoose.connection.once("open", () => {
-  console.log("Connected to MongoDB");
-});
+// // Check for DB connection
+// mongoose.connection.once("open", () => {
+//   console.log("Connected to MongoDB");
+// });
 
 // Start server
 app.listen(port, () => {
